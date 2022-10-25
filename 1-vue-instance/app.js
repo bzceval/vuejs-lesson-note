bd8e99f40b78f5ed7a74ad64eda2d4d5da6029bc
bzceval <busraaceval@gmail.com>
2022-10-25 15:52:35 +0300

    learned v-bind property

--- 1-vue-instance/app.js
+++ 1-vue-instance/app.js
@@ -1,13 +1,19 @@
 //vuejs ile kontrol etmek istediğim yere vuejs uygulamasını create etmeliyim 
 // Vue.createApp() fonksiyonu
 // Bu fonksiyon içerisinde vuejs uygulamasını daha anlamlı hale getirmek için bir constructor alır.
-// String interpolition: data içerisine yazılan key ile index içerisine yazdırabilirim.
-
+// 1- String interpolition: data içerisine yazılan key ile index içerisine yazdırabilirim.
+// String interpolitionu bir elementin text-contenti ile yaparız 
+// Fakat attribütüne yapamayız mesela a elementinin href içini
+// Bu durumu nasıl düzeltirim?
+// 2- v-bind
+// attribute değerini vuejs üzerinde bulunan ... data adıyla bağla
+// for exp: v-bind:href="url"   
 const app = Vue.createApp({
     data() {
         return {
             title: "Vuejs Bootcampinde 1. gün",
-            content: "Lorem ipsum dolar sit amet..."
+            content: "Lorem ipsum dolar sit amet...",
+            url: "https://www.google.com/teapot"
         }
     }
 }).mount("#app")
