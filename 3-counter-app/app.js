1d60ab59e187f8e21232ad845fdb68b8c3b2491e
bzceval <busraaceval@gmail.com>
2022-11-01 15:36:16 +0300

    computed with virtual dom

--- 3-counter-app/app.js
+++ 3-counter-app/app.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
    data() {
     return {
-        counter: 0
+        counter: 0,
+        counter1: 0,
+        counter2: 0
     }
    },
    methods: {
@@ -11,5 +13,23 @@ const app = Vue.createApp({
     azalt() {
         this.counter --;
     }
+   },
+   computed: {
+   // virtual domu kullanmak için kullanılır.
+   // bir fonksiyon gibi çalışan değişkenlerdir.
+   // computed içindeki değeri okuyabiliriz, ama set edemeyiz.
+   // reaktif olarak method gibi tanımlanan yapılardır.
+   // ilgili fonksiyon geldiğinde çalıştırır.
+   // geriye bir değer döndürmesi gerektiği için return kullanılır.
+   getCounter1Result() {
+    console.log("counter çalıştı")
+    // this.counter1 ++;
+    return this.counter1 > 5 ? "Büyük" : "Küçük"
+   },
+   getCounter2Result() {
+    console.log("counter çalıştı")
+    // this.counter1 ++;
+    return this.counter2 > 5 ? "Büyük" : "Küçük"
+   }
    }
 }).mount("#app")
